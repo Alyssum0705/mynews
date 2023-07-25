581cad9950e2b0001365a034514d17c6814b1f81
Arisa <bluelotus.melody@gmail.com>
2023-07-25 09:44:39 +0000

    課題　04 Routingについて理解する

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,20 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+use App\Http\Controllers\Admin\NewsController;
+Route::controller(NewsController::class)->prefix('admin')->group(function() {
+    Route::get('news/create', 'add');
+});
+
+//課題４
+use App\Http\Controllers\Admin\ProfileController;
+Route::controller(ProfileController::class)->prefix('admin')->group(function() {
+    Route::get('profile/create', 'add');
+    Route::get('profile/edit', 'edit');
+});
+
+// 課題３
+// Route::controller(AAA::class)->group(function(){
+//     Route::get('XXX', 'bbb')
+// });
