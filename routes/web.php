5841c87b65001db891f8b1657511ce9d2317f512
Arisa <bluelotus.melody@gmail.com>
2023-08-07 09:50:00 +0000

    課題　08 ニュース投稿画面を作成しよう

--- routes/web.php
+++ routes/web.php
@@ -22,18 +22,27 @@ Route::controller(NewsController::class)->prefix('admin')->name('admin.')->middl
     Route::get('news/create', 'add')->name('news.add');
     Route::post('news/create', 'create')->name('news.create');
 });
+// ↑↓
+// Route::get('/admin/news/create', [NewsController::class, 'add'])->middleware('auth')->name('admin.news.add');
+// Route::post('/admin/news/create', [NewsController::class, 'create'])->middleware('auth')->name('admin.news.create');
 
 //課題４
 use App\Http\Controllers\Admin\ProfileController;
-Route::controller(ProfileController::class)->prefix('admin')->middleware('auth')->group(function() {
+Route::controller(ProfileController::class)->prefix('admin')->name('admin.')->middleware('auth')->group(function() {
     Route::get('profile/create', 'add');
     Route::get('profile/edit', 'edit');
+    Route::post('profile/create', 'create')->name('profile.create');
+    Route::post('profile/edit', 'update')->name('profile.update');
 });
 
 // 課題３
 // Route::controller(AAA::class)->group(function(){
 //     Route::get('XXX', 'bbb')
 // });
+// ↑↓書き換えてみよう
+// use App\Http\Controllers\AAAContoller;
+// Route::get('XXX', [AAAContoller:class, 'bbb']);
+
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
