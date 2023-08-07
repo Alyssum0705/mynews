8c9048fc17feeb9af12276bcc2faf2b3e5bce15c
Arisa <bluelotus.melody@gmail.com>
2023-08-07 05:07:30 +0000

    課題　07 ユーザー認証を実装する

--- routes/web.php
+++ routes/web.php
@@ -24,7 +24,7 @@ Route::controller(NewsController::class)->prefix('admin')->group(function() {
 
 //課題４
 use App\Http\Controllers\Admin\ProfileController;
-Route::controller(ProfileController::class)->prefix('admin')->group(function() {
+Route::controller(ProfileController::class)->prefix('admin')->middleware('auth')->group(function() {
     Route::get('profile/create', 'add');
     Route::get('profile/edit', 'edit');
 });
