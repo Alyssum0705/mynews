31caca5b6e8078735a8d9f753ffa8198f1d40c3f
Arisa <bluelotus.melody@gmail.com>
2023-08-13 17:55:51 +0000

    課題　09 投稿データを保存しよう

--- app/Http/Controllers/Admin/ProfileController.php
+++ app/Http/Controllers/Admin/ProfileController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers\Admin;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
+use App\Models\Profile;
+
 class ProfileController extends Controller
 {
     //
@@ -15,6 +17,17 @@ class ProfileController extends Controller
 
     public function create(Request $request)
     {
+        $this->validate($request, Profile::$rules);
+
+        $profile = new Profile;
+        $form = $request->all();
+        
+        unset($form['_token']);
+        
+         // データベースに保存する
+        $profile->fill($form);
+        $profile->save();
+        
         return redirect('admin/profile/create');
     }
 
