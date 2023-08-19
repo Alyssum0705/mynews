a1798a73c39525cbebca5e275e2448dff10edb39
Arisa <bluelotus.melody@gmail.com>
2023-08-19 12:29:06 +0000

    課題　11 投稿したニュースを更新/削除しよう

--- routes/web.php
+++ routes/web.php
@@ -21,6 +21,10 @@ use App\Http\Controllers\Admin\NewsController;
 Route::controller(NewsController::class)->prefix('admin')->name('admin.')->middleware('auth')->group(function () {
     Route::get('news/create', 'add')->name('news.add');
     Route::post('news/create', 'create')->name('news.create');
+    Route::get('news', 'index')->name('news.index');
+    Route::get('news/edit', 'edit')->name('news.edit');
+    Route::post('news/edit', 'update')->name('news.update');
+    Route::get('news/delete', 'delete')->name('news.delete');
 });
 // ↑↓
 // Route::get('/admin/news/create', [NewsController::class, 'add'])->middleware('auth')->name('admin.news.add');
@@ -30,7 +34,7 @@ Route::controller(NewsController::class)->prefix('admin')->name('admin.')->middl
 use App\Http\Controllers\Admin\ProfileController;
 Route::controller(ProfileController::class)->prefix('admin')->name('admin.')->middleware('auth')->group(function() {
     Route::get('profile/create', 'add');
-    Route::get('profile/edit', 'edit');
+    Route::get('profile/edit', 'edit')->name('profile.edit');
     Route::post('profile/create', 'create')->name('profile.create');
     Route::post('profile/edit', 'update')->name('profile.update');
 });
